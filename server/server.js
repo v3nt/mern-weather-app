8bd0e5830ec9a906ae132586f2c998e360c2bc35
v3nt <dan@jynk.net>
2021-01-02 13:35:18 +0000

    server started

---
+++ server/server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = process.env.PORT;
+
+//use body parser to get data from POST requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Use API routes
+const apis = require("./api");
+app.use("/api", apis);
+
+app.listen(port, () => console.log(`listening on port ${port}`));
